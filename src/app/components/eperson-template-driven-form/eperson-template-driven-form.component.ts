e1b51f641572530c4bf28e1e1003a9854265694a
G.Paraskevaidis <giorgosparske@hotmail.com>
2024-04-08 19:46:02 +0300

    template driven-form

---
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { EPerson } from 'src/app/shared/interfaces/person';
+
+@Component({
+  selector: 'app-eperson-template-driven-form',
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
+  templateUrl: './eperson-template-driven-form.component.html',
+  styleUrl: './eperson-template-driven-form.component.css',
+})
+export class EpersonTemplateDrivenFormComponent {
+  @Output() eperson = new EventEmitter<EPerson>();
+  onSubmit(value: any) {
+    console.log(value as EPerson);
+    this.eperson.emit(value as EPerson);
+  }
+}
